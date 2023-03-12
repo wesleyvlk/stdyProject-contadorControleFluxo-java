15e9f935ca89c7f3e27e3bfdfb8112d70d18eaee
wesleyvlk <wesleymuniz20@gmail.com>
2023-03-12 20:53:33 -0300

    Feat: add Main to run CounterController

---
+++ src/com/dio/counterchallenge/Main.java
@@ -0,0 +1,9 @@
+package com.dio.counterchallenge;
+
+import com.dio.counterchallenge.controller.CounterController;
+
+public class Main {
+    public static void main(String[] args) {
+        new CounterController().counterView();
+    }
+}
